a8cc3800f48baaa50d4fb939aca81241e96f8fdb
Milad_Mrk <milad.rastgoou@gmail.com>
2022-04-11 21:47:17 +0430

    MiladR

--- src/App.js
+++ src/App.js
@@ -1,23 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+
+import "./App.css";
+import Comment from "./Components/Comment";
+import FullComment from "./Components/FullComment";
+import NewComment from "./Components/NewComment";
+import axios from "axios";
 
 function App() {
+  const [comment, setComment] = useState(null);
+  const [fullComment, setFullComment] = useState([]);
+
+  const clickHandler = (id) => {
+    console.log("id:" + id);
+    const set = comment.filter((i) => {
+      return i.id === id;
+    });
+    setFullComment(set);
+  };
+
+  useEffect(() => {
+    // هندل کردن با تن و کش
+    // axios
+    //   .get("https://jsonplaceholder.typicode.com/posts")
+    //   .then((res) => {
+    //     setComment(res.data.slice(0, 4));
+    //   })
+    //   .catch((eer) => {
+    //     console.log(eer);
+    //   });
+
+    // هندل کردن با ایسینگ اویت
+    const getComment = async () => {
+      try {
+        const allData = await axios.get(
+          "https://jsonplaceholder.typicode.com/posts"
+        );
+
+        setComment(allData.data.slice(0, 4));
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getComment();
+  }, []);
+
+  console.log(fullComment);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section className="boxTwo">
+        {comment ? (
+          comment.map((i) => {
+            return (
+              <Comment
+                key={i.id}
+                name={i.title}
+                email={i.id}
+                onClick={() => clickHandler(i.id)}
+              />
+            );
+          })
+        ) : (
+          <h3>Loding...</h3>
+        )}
+      </section>
+      <FullComment fullComment={fullComment} />
+      <NewComment />
     </div>
   );
 }
